b445a21d394b1c85c52d5763e5efdcdef2705a8f
vhsconnect <90valentin@gmail.com>
2022-02-27 17:34:11 +0100

    clean: move observables hook outside Main

--- src/Comps/Main.jsx
+++ src/Comps/Main.jsx
@@ -1,59 +1,17 @@
 import React from "react";
 import RadioList from "./RadioList";
 import Button from "./Button";
-import { fromEvent } from "rxjs";
-import { debounceTime } from "rxjs/operators";
+import useRegisterObservables from "../hooks/useRegisterObservables";
 
 export default function Main() {
+
   const [channels, setChannels] = React.useState([]);
   const [tag, setTag] = React.useState("");
   const [countrycode, setCountrycode] = React.useState("");
   const [name, setName] = React.useState("");
   const [favorites, setFavorites] = React.useState([]);
 
-  React.useEffect(() => {
-    const tagsObservable = fromEvent(document.getElementById("tags"), "input");
-    const ccObservable = fromEvent(
-      document.getElementById("countrycode"),
-      "input"
-    );
-    const nameObservable = fromEvent(document.getElementById("name"), "input");
-
-    tagsObservable
-      .pipe(debounceTime(1000))
-      .subscribe((e) => setTag(e.target.value));
-
-    ccObservable
-      .pipe(debounceTime(1000))
-      .subscribe((e) => setCountrycode(e.target.value));
-
-    nameObservable
-      .pipe(debounceTime(1000))
-      .subscribe((e) => setName(e.target.value));
-
-    const s1 = tagsObservable.subscribe(() => {
-      document.getElementById("countrycode").value = ""
-      document.getElementById("name").value = ""
-    });
-    const s2 = ccObservable.subscribe(() => {
-      document.getElementById("tags").value = ""
-      document.getElementById("name").value = ""
-    });
-    const s3 = nameObservable.subscribe(() => {
-      document.getElementById("tags").value = ""
-      document.getElementById("countrycode").value = ""
-    });
-
-    fetch("/favorites")
-      .then((data) => data.json())
-      .then(setFavorites);
-
-    return () => {
-      s1.unsubscribe();
-      s2.unsubscribe();
-      s3.unsubscribe();
-    };
-  }, []);
+  useRegisterObservables({ setTag, setCountrycode, setName, setFavorites })
 
   React.useEffect(() => {
     const searchField = tag
