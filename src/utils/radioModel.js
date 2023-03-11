e07793b26a0e5014a6e4c78c03b9569d899ebc5a
vhsconnect <90valentin@gmail.com>
2023-03-11 22:39:45 +0100

    restore query slugs on requested radio links

--- src/utils/radioModel.js
+++ src/utils/radioModel.js
@@ -4,14 +4,13 @@ subtract = R.flip(R.subtract)
 //solves the cached audio data problem in chromium
 //not firefox
 //side-effect 🚓
-//let slug = 0
+let slug = 0
 
 const radioModel = (v = []) => {
   const pub = {
     values: v,
     next(x) {
-      // const stream = new Audio(x.url_resolved + '?' + slug++)
-      const stream = new Audio(x.url_resolved)
+      const stream = new Audio(x.url_resolved + '?' + slug++)
       stream.preload = 'none'
       stream.volume = this.up() ? 0 : 1
       return radioModel(
