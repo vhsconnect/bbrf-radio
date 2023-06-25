fd9bf8c0c8a1cd7b20e16ed05fb33d47806e52be
Tony <42141705+vhsconnect@users.noreply.github.com>
2023-06-25 21:50:10 +0200

    update esbuild (#6)
    * update esbuild
    
    * precision: countrycode placeholder

--- package.nix
+++ package.nix
@@ -8,7 +8,7 @@
 
 stdenv.mkDerivation rec {
   pname = "bbrf-radio";
-  version = "0.4.2";
+  version = "0.5.1";
   src = builtins.path { path = ./.; name = "bbrf-radio"; };
 
   nativeBuildInputs = [
