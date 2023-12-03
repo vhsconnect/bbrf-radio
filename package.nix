a73377ef5cb5013939e1351ebac1ab45358070b4
Tony <42141705+vhsconnect@users.noreply.github.com>
2023-12-03 12:28:10 +0100

    0.6.1 (#9)
    * bugfix: migration to v1 apiVersion: handle storage
    
    * bump 0.6.1

--- package.nix
+++ package.nix
@@ -8,7 +8,7 @@
 
 stdenv.mkDerivation rec {
   pname = "bbrf-radio";
-  version = "0.6.0";
+  version = "0.6.1";
   src = builtins.path { path = ./.; name = "bbrf-radio"; };
 
   nativeBuildInputs = [
