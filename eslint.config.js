a3738f7808aff50f8291df734a3b8dafcb121876
vhsconnect <90valentin@gmail.com>
2025-04-13 13:38:07 +0200

    eslint configuration - react

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,27 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: '17',
+      },
+    },
+    rules: {
+      'react/prop-types': 'off',
+      'react/jsx-uses-vars': 'error',
+      'react/jsx-uses-react': 'error',
+    },
+  },
+]
