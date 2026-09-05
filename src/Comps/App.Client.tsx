3324b70de26a5579b5c73845f5a01afcee6d13a0
vhsconnect <90valentin@gmail.com>
2026-09-05 14:38:23 +0200

    fix - teleprompt crash on mixed content

--- src/Comps/App.Client.tsx
+++ src/Comps/App.Client.tsx
@@ -1,33 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
-import * as R from 'ramda'
 import Main from './Main'
-
-const cloudflareDns = 'https://cloudflare-dns.com/dns-query'
-const radioBrowserRequest = () =>
-  fetch(cloudflareDns + '?name=_api._tcp.radio-browser.info' + '&type=SRV', {
-    headers: {
-      Accept: 'application/dns-json',
-    },
-  })
+import { discoverRadioBrowserApiUrl } from '../utils/radioBrowserDiscovery'
 
 const App = () => {
-  const [radioBrowserApiUrl, setRadioBrowserApiUrl] = useState(null)
+  const [radioBrowserApiUrl, setRadioBrowserApiUrl] = useState<string | null>(
+    null
+  )
 
   useEffect(() => {
-    radioBrowserRequest()
-      .then(x => x.json())
-      .then(
-        R.pipe(
-          R.prop('Answer'),
-          R.head,
-          // @ts-expect-error
-          R.prop('data'),
-          R.split(' '),
-          x => x[3],
-          setRadioBrowserApiUrl
-        )
-      )
+    discoverRadioBrowserApiUrl().then(setRadioBrowserApiUrl)
   }, [])
 
   return (
